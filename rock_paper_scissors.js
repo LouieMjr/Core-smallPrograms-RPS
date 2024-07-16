2f5c479a96e5ad615da313c5cf5f7941d3051711
Louie Mendez <lmendezjr024@gmail.com>
2024-07-16 22:29:15 +0200

    added player choice as function

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -6,19 +6,32 @@ function prompt(message) {
   console.log(` => ${message}`);
 }
 
-prompt(`Choose one: ${OPTIONS.join(', ')}`);
-let choice = ask();
-choice = choice[0].toUpperCase() + choice.slice(1);
+const playerChoice = function (array) {
+  prompt(`Choose one: ${array.join(', ')}`);
+  let choice = ask();
+  choice = choice[0].toUpperCase() + choice.slice(1);
 
-while (!OPTIONS.includes(choice)) {
-  prompt(`Thats not a valid choice. Pick between ${OPTIONS.join(', ')}`);
-  choice = ask();
-}
-
-const computerChoice = function (array) {
-  const index = Math.round(Math.random() * OPTIONS.length);
-  return `Computer choose ${array[index]}`;
+  while (!array.includes(choice)) {
+    prompt(`Thats not a valid choice. Pick between ${array.join(', ')}`);
+    choice = ask();
+  }
+  return choice;
 };
 
-console.log(computerChoice(OPTIONS));
+const computerChoice = function (array) {
+  const randomIndex = Math.floor(Math.random() * OPTIONS.length);
+  const computerPick = array[randomIndex];
+  console.log(randomIndex);
+  return computerPick;
+};
+
+const determineWinner = function (player, computer) {
+  const playerPick = player(OPTIONS)
+  const computerPick = computer(OPTIONS)
+  console.log(`You choose ${playerPick}`);
+  console.log(`Computer choose ${computerPick}`);
+  return;
+};
+
+determineWinner(playerChoice, computerChoice);
 
