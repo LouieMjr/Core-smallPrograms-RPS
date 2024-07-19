43b68b9472de040f4a0c10e2ea5d494a16bc3656
Louie Mendez <lmendezjr024@gmail.com>
2024-07-19 09:50:35 +0200

    ability to keep score and determine a winner after someone wins 3 rounds. Added increment functions, simplified other functions and using global variables for the score

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,8 +1,10 @@
 const readline = require('readline-sync');
 const gameMessages = require('./game_messages.json');
 const ask = readline.question;
-const IN_GAME_OPTIONS = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'];
-const ABBREVIATED_IN_GAME_OPTIONS = ['R', 'P', 'Sc', 'Sp', 'L'];
+const GAME_OPTIONS = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard'];
+const ABBREVIATED_GAME_OPTIONS = ['R', 'P', 'Sc', 'Sp', 'L'];
+let playerScore = 0;
+let computerScore = 0;
 
 function prompt(message) {
   console.log(` => ${message}`);
@@ -45,8 +47,15 @@ const computerChoice = function (array) {
   return computerPick;
 };
 
+const incrementPlayerScore = function (score) {
+  return ++score;
+};
+const incrementComputerScore = function (score) {
+  return ++score;
+};
 
 const determineWinner = function (playerChoice, computerChoice) {
+  console.clear();
   prompt(`You choose ${playerChoice}, Computer choose ${computerChoice}`);
   if (
     (playerChoice === 'Lizard' && (computerChoice === 'Spock' || computerChoice === 'Paper')) ||
@@ -54,21 +63,42 @@ const determineWinner = function (playerChoice, computerChoice) {
     (playerChoice === 'Rock' && (computerChoice === 'Scissors' || computerChoice === 'Lizard')) ||
     (playerChoice === 'Paper' && (computerChoice === 'Rock' || computerChoice === 'Spock')) ||
     (playerChoice === 'Scissors' && (computerChoice === 'Paper' || computerChoice === 'Lizard'))) {
-    prompt(`${playerChoice} beats ${computerChoice}! You win!`);
+    prompt(`${playerChoice} beats ${computerChoice}! You win the round!`);
+    return 'player';
   } else if (playerChoice === computerChoice) {
     prompt('Its a tie');
   } else {
-    prompt(`${computerChoice} beats ${playerChoice}! Computer wins!`);
+    prompt(`${computerChoice} beats ${playerChoice}! Computer wins this round!`);
+    return 'computer';
   }
-  return restartGame();
+};
+
+const displayScore = function (playerPoints, computerPoints) {
+  if (playerPoints === 3) {
+    return prompt(`You win the game ${playerPoints} to ${computerPoints}`);
+  } else if (computerPoints === 3) {
+    return prompt(`Computer wins the game ${computerPoints} to ${playerPoints}`);
+  } else if (playerPoints === computerPoints) {
+    prompt(`The score is tied ${playerPoints} to ${computerPoints}`);
+  } else if (playerPoints > computerPoints) {
+    prompt(`You're winning ${playerPoints} to ${computerPoints}`);
+  } else {
+    prompt(`You're losing ${playerPoints} to ${computerPoints}`);
+  }
 };
 
 const startGame = function () {
   console.clear();
   prompt(gameMessages.welcome);
-  const playerPick = playerChoice(IN_GAME_OPTIONS, ABBREVIATED_IN_GAME_OPTIONS);
-  const computerPick = computerChoice(IN_GAME_OPTIONS);
-  determineWinner(playerPick, computerPick);
+  displayScore(playerScore, computerScore);
+  while (playerScore < 3 && computerScore < 3) {
+    const playerPick = playerChoice(GAME_OPTIONS, ABBREVIATED_GAME_OPTIONS);
+    const computerPick = computerChoice(GAME_OPTIONS);
+    const winner = determineWinner(playerPick, computerPick);
+    if (winner === 'player') playerScore = incrementPlayerScore(playerScore);
+    if (winner === 'computer') computerScore = incrementComputerScore(computerScore);
+    displayScore(playerScore, computerScore);
+  }
 };
 
 const restartGame = function () {
@@ -79,8 +109,11 @@ const restartGame = function () {
     prompt(gameMessages.invalidRestart);
     response = ask().toUpperCase();
   }
+  playerScore = 0;
+  computerScore = 0;
   if (response === 'YES' || response === 'Y') return startGame();
   if (response === 'NO' || response === 'N') return prompt(gameMessages.goodbye);
 };
 
 startGame();
+restartGame();
